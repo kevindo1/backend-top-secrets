34b4a15084bebf91d5501c4752e9cf8b58d7d802
Kevin Do <kevind0501@gmail.com>
2022-03-29 11:44:31 -0700

    added mock user for testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,14 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+// Dummy user for testing
+const mockUser = {
+  firstName: 'Mock',
+  lastName: 'User',
+  email: 'test@example.com',
+  password: '12345678',
+};
+
 describe('. routes', () => {
   beforeEach(() => {
     return setup(pool);
