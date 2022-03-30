33cd33b3522f5036e523c4069182c7d4378d275b
Kevin Do <kevind0501@gmail.com>
2022-03-30 14:49:12 -0700

    seeded data into SQL

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,7 +1,8 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS secrets CASCADE;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -15,3 +16,9 @@ CREATE TABLE secrets (
     description TEXT NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO
+  secrets (title, description)
+VALUES
+  ('secret1', 'this is the first secret'),
+  ('secret2', 'this is the second secret')
